64e480f44a328abed83782010efaa37ded853eec
Hieu Phan <hhphan@umass.edu>
2020-05-06 14:37:49 -0400

    update script

--- scripts/launch_bert.sh
+++ scripts/launch_bert.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir results
-sbatch --partition=1080ti-short --gres=gpu:1 --mem=8192 --output=results/bert_%j.log scripts/bert.sh
+sbatch --partition=2080ti-short --gres=gpu:1 --mem=8192 --output=results/bert_%j.log scripts/bert.sh
